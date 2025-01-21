c626006619d830dda189fe2eefc12a6ac6397315
hoshiirui <shibaharumii@gmail.com>
2025-01-21 10:45:12 +0800

    adding insight

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,27 @@ import "./App.css";
 import Homepage from "./pages/Homepage";
 import NavBar from "./components/NavBar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import GalleryDetail from "./pages/GalleryDetail";
-import Stories from "./pages/Stories";
-import StoryDetail from "./pages/StoryDetail";
 import AboutUs from "./pages/AboutUs";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/home" element={<Homepage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        {/* <Route path="/stories" element={<Stories />} />
+    <>
+      <Analytics />
+      <SpeedInsights />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          {/* <Route path="/stories" element={<Stories />} />
         <Route path="gallery/:id" element={<GalleryDetail />} />
         <Route path="/story/:id" element={<StoryDetail />} /> */}
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
